b4ccbc9c2b2a108dec5b40618a396208f76920f2
Michael Ferger <mferger92@gmail.com>
2016-08-04 02:03:42 -0600

    added key value pairs to add row object, fixed spacing in controller

--- src/js/controllers/HomeController.js
+++ src/js/controllers/HomeController.js
@@ -1,100 +1,109 @@
-var HomeController = function ($scope) {
+var HomeController = function($scope) {
     $scope.tableRows = [];
     $scope.users = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
     $scope.step2Instructions = true;
     $scope.step2 = false;
     $scope.total = 0;
     $scope.person = {
-      'value': 0,
+        'value': 0,
     }
     $scope.setRows = function(num) {
-      if($scope.person.value == 0){
-        $scope.tableRows = [];
-        for (var i = 0; i < num; i++) {
-          $scope.tableRows[i] = {
-            "oral": false,
-            "cleaning": false,
-            "filling": false,
-            "xray": false,
-            "crown": false,
-            "canal": false,
-            "dentures": false,
-            "braces": false,
-            "visible": false,
-            "subtotal": 0
-          };
-          $scope.person.value = num;
+        if ($scope.person.value == 0) {
+            $scope.tableRows = [];
+            for (var i = 0; i < num; i++) {
+                $scope.tableRows[i] = {
+                    "oral": false,
+                    "cleaning": false,
+                    "filling": false,
+                    "xray": false,
+                    "crown": false,
+                    "canal": false,
+                    "dentures": false,
+                    "braces": false,
+                    "subtotal": 0
+                };
+                $scope.person.value = num;
+            }
+        } else {
+            return;
         }
-      } else {
-        return;
-      }
-      $scope.step2Instructions = false;
-      $scope.step2 = true;
+        $scope.step2Instructions = false;
+        $scope.step2 = true;
     }
     $scope.selectAll = function(procedure, checked) {
-      if(checked == true) {
-    angular.forEach($scope.tableRows, function(person, index) {
-      if (person[procedure] == false) {
-        person[procedure] = true;
-      } else if (person[procedure] == true) {
-        return
-      }
-      $scope.checked($scope.prices[procedure], index, checked, procedure);
-    });
-  } else if(checked == false) {
-    angular.forEach($scope.tableRows, function(person, index) {
-      $scope.checked($scope.prices[procedure], index, checked, procedure);
-    });
-  }
-};
-    $scope.addRow = function () {
-      $scope.tableRows.push({subtotal: 0})
+        if (checked == true) {
+            angular.forEach($scope.tableRows, function(person, index) {
+                if (person[procedure] == false) {
+                    person[procedure] = true;
+                } else if (person[procedure] == true) {
+                    return
+                }
+                $scope.checked($scope.prices[procedure], index, checked, procedure);
+            });
+        } else if (checked == false) {
+            angular.forEach($scope.tableRows, function(person, index) {
+                $scope.checked($scope.prices[procedure], index, checked, procedure);
+            });
+        }
+    };
+    $scope.addRow = function() {
+        $scope.tableRows.push({
+          "oral": false,
+          "cleaning": false,
+          "filling": false,
+          "xray": false,
+          "crown": false,
+          "canal": false,
+          "dentures": false,
+          "braces": false,
+          "subtotal": 0
+        })
     }
     $scope.prices = {
-            oral: 100,
-            cleaning: 50,
-            filling: 75,
-            xray: 200,
-            crown: 125,
-            canal: 100,
-            dentures: 60,
-            braces: 130
-        }
+        oral: 100,
+        cleaning: 50,
+        filling: 75,
+        xray: 200,
+        crown: 125,
+        canal: 100,
+        dentures: 60,
+        braces: 130
+    }
     $scope.selected = {
         value: 0
     };
-    $scope.totalSavings = function(){
-      var total = 0;
-      angular.forEach($scope.tableRows, function (values, index) {
-        total += values.subtotal;
-        $scope.total = total;
-      })
-      return total;
+    $scope.totalSavings = function() {
+        var total = 0;
+        angular.forEach($scope.tableRows, function(values, index) {
+            total += values.subtotal;
+            $scope.total = total;
+        })
+        return total;
     }
     $scope.dollarAmmount = 0.00;
     $scope.checked = function(amount, position, checked, procedure) {
         if (checked) {
-          angular.forEach($scope.tableRows, function (person, index) {
-            if (index == position) {
-              $scope.tableRows[position][procedure] = true;
-            }
-          })
+            angular.forEach($scope.tableRows, function(person, index) {
+                if (index == position) {
+                    $scope.tableRows[position][procedure] = true;
+                }
+            })
             $scope.dollarAmmount = $scope.dollarAmmount + amount;
             $scope.tableRows[position].subtotal += amount;
-        } else if(!checked){
-          angular.forEach($scope.tableRows, function (person, index) {
-            if (index == position) {
-              $scope.tableRows[position][procedure] = false;
-            }
-          })
+        } else if (!checked) {
+            angular.forEach($scope.tableRows, function(person, index) {
+                if (index == position) {
+                    $scope.tableRows[position][procedure] = false;
+                }
+            })
             $scope.dollarAmmount = $scope.dollarAmmount - amount;
             $scope.tableRows[position].subtotal -= amount;
         }
     };
-    $scope.moneyWorth = function (amount) {
-      var movieTickets = 7;
-      var total = Math.round(amount/movieTickets);
-      return total;
+    $scope.moneyWorth = function(amount) {
+        var movieTickets = 7;
+        var total = Math.round(amount / movieTickets);
+        return total;
     }
 };
 
